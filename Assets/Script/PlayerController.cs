34ae5daa9d391258bd53a486bd73af236385e7f5
Myoungcholho <110521729+Myoungcholho@users.noreply.github.com>
2023-11-16 20:21:30 +0900

    명철호
    이동, 애니메이션

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
 
     private Rigidbody2D rigid;
     private Vector2 inputVec;
     private Animator animator;
 
-    private float v;
+    private float horizontal;
+    private bool isGround;
+    private int jumpCount = 0;
 
     private void Awake()
     {
@@ -21,37 +24,74 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();       
+        MoveInput();
+        OnPlayAnim();
+
+        //Debug.Log("JumpCount : " + jumpCount);
     }
 
     private void FixedUpdate()
     {
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
-        rigid.MovePosition(rigid.position + nextVec);
-        animator.SetInteger("hRaw", (int)v);
+        MoveVelocity();
     }
 
-    private void Move()
+    private void MoveInput()
     {
-        if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(Key.dictionary[KeyAction.P1_LEFT]))
         {
-            inputVec.x = -1;
-            v = -1;
-            animator.SetBool("Moving", true);
+            horizontal = -1;
         }
-        else if(Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(Key.dictionary[KeyAction.P1_RIGHT]))
         {
-            inputVec.x = 1;
-            v = 1;
-            animator.SetBool("Moving", true);
+            horizontal = 1;
         }
         else
         {
-            inputVec.x = 0;
-            v = 0;
-            animator.SetBool("Moving", false);
+            horizontal = 0;
+        }
+
+        if (Input.GetKey(Key.dictionary[KeyAction.P1_JUMP]) && jumpCount < 1)
+        {
+            jumpCount++;
+            rigid.velocity = Vector2.zero;
+            rigid.AddForce(new Vector2(0, jumpForce));
         }
     }
 
+    private void MoveVelocity()
+    {
+        rigid.velocity = new Vector2(horizontal * speed , rigid.velocity.y);
+    }
 
+    private void OnPlayAnim()
+    {
+        if (animator.GetInteger("hRaw") != horizontal)
+        {
+            animator.SetInteger("hRaw", (int)horizontal);
+        }
+        else if (animator.GetBool("isGround") != isGround)
+        {
+            animator.SetBool("isGround", isGround);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+        {
+            if (collision.contacts[0].normal.y > 0.7f)
+            {
+                isGround = true;
+                jumpCount = 0;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+        {
+            isGround = false;
+        }
+    }
 }
