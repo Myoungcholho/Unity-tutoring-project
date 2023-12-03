a4a30cfdb547a4785e0f26a2adf9c88b0ad49d6b
waterlsk <lskcindy020829@gmail.com>
2023-12-03 15:53:02 +0900

    2차
    점프랑 앞뒤로 움직이는 것까지는 했는데 점프가 순간이동하는 것처럼 되고 다른 버튼 눌렀을 때 멈추는 게 안되서 혼자힘으로는 지금으로썬 이게 최선인듯..? 뭔가를 참고해야 고칠 수 있을 것 같기도..

--- Assets/lsk/P1_Controller.cs
+++ Assets/lsk/P1_Controller.cs
@@ -1,25 +1,77 @@
 using System.Collections; //
 using System.Collections.Generic; //
 using System.Collections.Specialized;
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
 using UnityEngine; //
 
 public class P1_Controller : MonoBehaviour
 {
-    private Rigidbody rb;
+    private Rigidbody2D rigid;
+    private SpriteRenderer spriteRenderer;
+
+    public float maxspeed;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + maxspeed * horizontal;
         transform.position = position;
+
+        /*
+         if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if(Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                horizontal = 0;
+                position.x = position.x - maxspeed * horizontal;
+                transform.position = position;
+            }
+            else
+            {
+                position.x = position.x - maxspeed * horizontal;
+                transform.position = position;
+            }
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                horizontal = 0;
+                position.x = position.x - maxspeed * horizontal;
+                transform.position = position;
+            }
+            else
+            {
+                position.x = position.x - maxspeed * horizontal;
+                transform.position = position;
+            }
+        }
+        */
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            position.y = position.y + 1; //서서히 올라가지 않고 한칸 위로 순간이동 함..
+            transform.position = position;
+        }
+
+        if (horizontal == -1.0)
+        {
+            spriteRenderer.flipX = true;
+
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 }
