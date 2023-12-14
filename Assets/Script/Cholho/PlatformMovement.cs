6c8586e47b963ec0599b45d285da428b916e2cb0
Myoungcholho <110521729+Myoungcholho@users.noreply.github.com>
2023-12-15 01:27:33 +0900

    명철호
    움직이는 발판

---
+++ Assets/Script/Cholho/PlatformMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    public float speed;
+
+    public Transform startPos;
+    public Transform endPos;
+    private Transform destPos;
+
+    void Start()
+    {
+        transform.position = startPos.position;
+        destPos = endPos;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, destPos.position, Time.deltaTime * speed);
+        float distance = Vector2.Distance(transform.position, destPos.position);
+        Debug.Log("transform.position : " + transform.position + "/ destPos.position : " + destPos.position + "/ distance : " + distance);
+        if (distance <= 0.05f)
+        {
+            if(destPos == endPos)
+            {
+                destPos = startPos;
+            }
+            else
+            {
+                destPos = endPos;
+            }
+        }
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.CompareTag("Player"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.transform.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
