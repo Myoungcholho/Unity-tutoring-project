c59dccd6f3691079b7db4eb5e23d787827d0f06b
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2023-12-24 20:30:47 +0900

    이동현
    23.12.24 commit  움직임 점프 애니메이션(점프제외)

--- Assets/ldh/Scripts/ldhPlayer.cs
+++ Assets/ldh/Scripts/ldhPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.U2D;
 using UnityEngine;
 
 
@@ -12,10 +13,10 @@ public class ldhPlayer : MonoBehaviour
     SpriteRenderer spriteRenderer;
 
     [SerializeField]
-    private float speed = 3;
+    private float speed = 3f;
 
     [SerializeField]
-    private float power = 3;
+    private float power = 3f;
 
     Rigidbody2D rb;
 
@@ -52,24 +53,45 @@ public class ldhPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        dir.x = Input.GetAxisRaw("Horizontal");    //GetAxis => unity에서 지원하는 방향키? 축을 가져온다. 그걸로 이동.
-        dir.y = Input.GetAxisRaw("Vertical");
-        dir = new Vector3(dir.x, dir.y, 0);
+        // transform.position += new Vector3(dir.x, 0, 0) * speed * Time.deltaTime; 
+
+        //dir.x = Input.GetAxisRaw("Horizontal");    //GetAxis => unity에서 지원하는 방향키? 축을 가져온다. 그걸로 이동.
+        //dir.y = Input.GetAxisRaw("Vertical");     수직 w와 s키
+
+        dir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir = Vector3.left;
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            dir = Vector3.right;
+        }
 
         if (Input.GetKeyDown(KeyCode.Space) && isGround)
             rb.AddForce(jump * power, ForceMode2D.Impulse);
-
-
+            
     }
 
 
     private void FixedUpdate()
     {
-        gameObject.transform.position += dir * speed * Time.fixedDeltaTime;
-        animator.SetFloat("speed", dir.magnitude);  //magnitude => 벡터 길이반환
-        if(dir.x != 0)
+        if(dir != Vector3.zero)
         {
+            transform.position += dir.normalized * speed * Time.fixedDeltaTime;
+            animator.SetFloat("speed", speed);
             spriteRenderer.flipX = dir.x < 0;
         }
+        else
+        {
+            animator.SetFloat("speed", 0f);
+        }
+        //transform.position += dir * speed * Time.fixedDeltaTime;
+        //animator.SetFloat("speed", dir.magnitude);  //magnitude => 벡터 길이반환
+        //if (dir.x != 0)
+        //{
+        //    spriteRenderer.flipX = dir.x < 0;
+        //}
     }
 }
