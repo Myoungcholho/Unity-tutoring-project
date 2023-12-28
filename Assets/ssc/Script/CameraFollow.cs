408caf5123595971c56cb15d57c9e114f5ce5a52
jelly0211 <ssc7347@gmail.com>
2023-12-28 19:58:33 +0900

    2
    CameraFollow스크립트 추가

---
+++ Assets/ssc/Script/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private Vector3 velocity;
+    [SerializeField]
+    private float smoothTime = 0.15f;
+
+    [SerializeField]
+    private Transform target;
+
+    void FixedUpdate()
+    {
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
