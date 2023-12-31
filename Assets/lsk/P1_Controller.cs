23de228517809357e7b8af40fdb048c60619d84e
waterlsk <lskcindy020829@gmail.com>
2023-12-31 19:18:37 +0900

    5차
    a를 누르다가 때면 바로 오른쪽을 보던 오류 수정, velocity를 안 쓰고 움직이도록 수정, but 점프가 안됨

--- Assets/lsk/P1_Controller.cs
+++ Assets/lsk/P1_Controller.cs
@@ -14,6 +14,8 @@ public class P1_Controller : MonoBehaviour
 
     public float maxspeed;
     public bool jumping = false;
+    public bool moving = false;
+    public LayerMask Tilemap;
 
     // Start is called before the first frame update
     void Start()
@@ -25,13 +27,9 @@ public class P1_Controller : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            if (Mathf.Abs(rigid.velocity.y) < 0.01f)
-            {
-                rigid.AddForce(new Vector2(0, 400));
-                jumping = true;
-            }
+            Jump();
         }
     }
 
@@ -41,6 +39,12 @@ public class P1_Controller : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         Debug.Log(horizontal);
 
+        Move(horizontal); //걷기 애니메이션 제어
+        UpdateAnimations(horizontal); //걷기 및 점프 애니메이션 제어
+    }
+
+    void Move(float horizontal)
+    {
         Vector2 position = transform.position;
 
         if (horizontal > 0)
@@ -50,7 +54,7 @@ public class P1_Controller : MonoBehaviour
                 horizontal = 0;
             }
         }
-        else if(horizontal < 0)
+        else if (horizontal < 0)
         {
             if (Input.GetKey(KeyCode.D))
             {
@@ -61,21 +65,55 @@ public class P1_Controller : MonoBehaviour
         position.x = position.x + maxspeed * horizontal;
         transform.position = position;
 
-        if (horizontal == -1.0)
+        if (horizontal == -1.0f)
         {
-            spriteRenderer.flipX = true;
-
+            // horizontal이 -1일 때 (좌측으로 이동)
+            spriteRenderer.flipX = true; // 스프라이트를 좌우로 뒤집어 좌측 방향으로 향하도록 설정
+            moving = true; // 이동 중임을 나타내는 플래그를 true로 설정
+        }
+        else if (horizontal == 1.0f)
+        {
+            //우측이동
+            spriteRenderer.flipX = false; //스프라이트 원래대로 돌람
+            moving = true; //이동 중임을 나타냄
         }
         else
         {
-            spriteRenderer.flipX = false;
+            //이동하지 않을 때 멈춰있는 모션
+            moving = false; //이동 중이 아님
+        }
+    }
+
+    void Jump()
+    {
+        //y 속도가 0일때만 점프 가능하도록 설정
+        rigid.AddForce(new Vector2(0, 800));
+        jumping = true;
+    }
+
+    bool IsGrounded()
+    {
+        float circleRadius = 0.2f;
+        Vector2 circleCenter = new Vector2(transform.position.x, transform.position.y - 0.1f);
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(circleCenter, circleRadius, Tilemap);
+
+        foreach (var collider in colliders)
+        {
+            if (collider.gameObject != gameObject)
+            {
+                jumping = false;
+                return true;
+            }
         }
 
-        animator.SetBool("Walking", Mathf.Abs(horizontal) > 0.01f);
+        return false;
+    }
+
+    void UpdateAnimations(float horizontal)
+    {
+        animator.SetBool("Walking", moving);
         animator.SetBool("Jumping", jumping);
-        if (rigid.velocity.y == 0)
-        {
-            jumping = false;
-        }
+        animator.SetFloat("VerticalSpeed", jumping ? 1f : 0f);
     }
 }
