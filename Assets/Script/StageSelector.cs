122f79a59dbeed44ef64a3fa83873d413475a39a
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-02-04 20:43:58 +0900

    image 마우스 hover시 확대

--- Assets/Script/StageSelector.cs
+++ Assets/Script/StageSelector.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class StageSelector : MonoBehaviour
+public class StageSelector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+
+    
+    // 이미지의 크기를 조절하는 변수
+    public float hoverScaleFactor = 1.2f;
+
+    private Vector3 originalSize;
+
+    public Button[] stageButton;
+    public GameObject[] levelPanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalSize = transform.localScale;
+
+        for(int i=0; i < levelPanel.Length; i++)
+        {
+            levelPanel[i].SetActive(false);
+        }
     }
-    public void OpenScene()
+
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        SceneManager.LoadScene("Stage 1");
+        //마우스가 이미지 위에 올라갔을때 이미지 크기 조절
+        transform.localScale = originalSize * hoverScaleFactor;
+    }
+
+    public void OnPointerExit(PointerEventData evenData)
+    {
+        //마우스가 이미지 벗어났을때 원래대로
+        transform.localScale = originalSize;
+    }
+ 
+    public void StageButtonClick(int stageButton)
+    {
+        for (int i=0; i < levelPanel.Length; i++)
+        {
+            levelPanel[i].SetActive(false);
+        }
+        levelPanel[stageButton].SetActive(true);
     }
 
     // Update is called once per frame
