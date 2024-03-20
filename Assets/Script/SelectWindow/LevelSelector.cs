9f4a2070ed4c04d6c88bc2211b3533a4e6ff05d6
Myoungcholho <110521729+Myoungcholho@users.noreply.github.com>
2024-03-21 02:28:24 +0900

    명철호
    level 클리어시 활성화로 변경

--- Assets/Script/SelectWindow/LevelSelector.cs
+++ Assets/Script/SelectWindow/LevelSelector.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -5,28 +6,90 @@ using UnityEngine.UI;
 public class LevelSelector : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public Image borderImg;
+    public Image crownImg;
     public int stageNumber;
+    private Image img;
+    private bool stageSelectable;
+    private bool isCleared;
+
+    private void Awake()
+    {
+        img = GetComponent<Image>();
+    }
 
     void Start()
     {
         borderImg.enabled = false;
+
+        int stageSelectIdx = stageNumber-1 >= 0 ? stageNumber-1 : 0;
+        int clearedIdx = stageNumber - 2 >= 0 ? stageNumber - 2 : 0;
+
+        stageSelectable = StageManager.instance.isStageCleared[stageSelectIdx];
+        isCleared = StageManager.instance.isStageCleared[clearedIdx];
+
+        StageManager.instance.isStageCleared.OnCleared += UpdateIsCleared;
+
+        CrownRendering();
+        UpdateAlpha();
     }
 
     // 클릭 시 실행할 동작을 여기에 추가
     public void OnPointerClick(PointerEventData eventData)
     {
-        
+        if (!stageSelectable)
+            return;
+
+        StageManager.instance.LevelNum = stageNumber;
     }
 
     //마우스가 이미지 위에 올라갔을때
     public void OnPointerEnter(PointerEventData eventData)
     {
+        if (!stageSelectable)
+            return;
+
         borderImg.enabled = true;
     }
 
     //마우스가 이미지 벗어났을때 원래대로
     public void OnPointerExit(PointerEventData evenData)
     {
+        if (!stageSelectable)
+            return;
+
         borderImg.enabled = false;
+    }
+
+    private void UpdateIsCleared(int num,bool value)
+    {
+        // 현재 스테이지가 호출된게 아니라면 return
+        if (num != stageNumber)
+            return;
+
+        isCleared = value;
+        int stageSelectIdx = stageNumber - 1 >= 0 ? stageNumber - 1 : 0;
+        int clearedIdx = stageNumber - 2 >= 0 ? stageNumber - 2 : 0;
+        stageSelectable = StageManager.instance.isStageCleared[stageSelectIdx];
+        isCleared = StageManager.instance.isStageCleared[clearedIdx];
+        CrownRendering();
+        UpdateAlpha();
+    }
+
+    private void CrownRendering()
+    {
+        crownImg.enabled = isCleared;
+    }
+    private void UpdateAlpha()
+    {
+        Color color = img.color;
+        if (isCleared)
+        {
+            color.a = 255f / 255f;
+        }
+        else
+        {
+            color.a = 130f / 255f;
+        }
+        img.color = color;
     }
 }
--- Assets/Script/SelectWindow/StageManager.cs
+++ Assets/Script/SelectWindow/StageManager.cs
@@ -1,9 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
+[System.Serializable]
+public class IsStageCleared
+{
+    public bool[] temp;
+    public event Action<int,bool> OnCleared;
+    public bool this[int index]
+    {
+        get
+        {
+            return temp[index];
+        }
+        set
+        {
+            temp[index] = value;
+            OnCleared?.Invoke(index + 1,value);
+        }
+    }
+    public IsStageCleared(int num)
+    {
+        temp = new bool[num];
+    }
+}
 
 public class StageManager : MonoBehaviour
 {
@@ -21,10 +45,28 @@ public class StageManager : MonoBehaviour
     }
     public Text stageText;
     public RectTransform[] line;
-    public bool[] isStageCleared;
+    public int stageCount;
     public float duration;          //너비가 늘어나는데 걸리는 시간
     public float targetWidth;       //목표로 하는 너비
     private int stageNum;
+    private int levelNum;
+
+    // level 이 셋팅 되면 호출할 이벤트 함수
+    [SerializeField]
+    public IsStageCleared isStageCleared;
+
+    private void Awake()
+    {
+        isStageCleared = new IsStageCleared(stageCount);
+    }
+
+    private void Start()
+    {
+        DecideDrawClearLine();
+        ShowProgressOnClear(0);
+    }
+
+    // 프로퍼티
     public int StageNum
     {
         get
@@ -37,14 +79,19 @@ public class StageManager : MonoBehaviour
             Debug.Log("StageNum Setting :" + stageNum);
         }
     }
-    public int levelNum { get; set; }
-
-
-    private void Start()
-    {
-        DecideDrawClearLine();
-        ShowProgressOnClear(0);
+    public int LevelNum 
+    { 
+        get
+        {
+            return levelNum;
+        }
+        set
+        {
+            levelNum = value;
+            Debug.Log("Level Setting :" + levelNum);
+        }
     }
+
     // 어떤 스테이지인지 TextUI를 업데이트합니다.
     public void StageTextUpdate(string str)
     {
@@ -63,7 +110,7 @@ public class StageManager : MonoBehaviour
     // 클리어 유무에 따라 선을 그릴지 결정합니다.
     private void DecideDrawClearLine()
     {
-        for(int i=0; i< isStageCleared.Length; i++)
+        for(int i=0; i< line.Length; i++)
         {
             if (isStageCleared[i])
             {
